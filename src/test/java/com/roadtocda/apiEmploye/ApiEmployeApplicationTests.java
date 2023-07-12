8108c4e79f9205762d93a90cdcc2658fff782df6
Theo Pierson <thheopierson@gmail.com>
2023-07-12 17:23:23 +0200

    build: apiEmploye project exercice

---
+++ src/test/java/com/roadtocda/apiEmploye/ApiEmployeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadtocda.apiEmploye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmployeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
